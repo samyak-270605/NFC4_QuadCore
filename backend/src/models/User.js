c8a3c75e6a4640b9ad86e50edc60b526a6314fc3
Hardik-M-Agarwal <hardikag04@gmail.com>
2025-08-05 16:41:29 +0530

    model creation

---
+++ backend/src/models/User.js
@@ -0,0 +1,100 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  profilePic: {
+    type: String,
+    default: "",
+  },
+  nativeLanguage: {
+    type: String,
+    default: "",
+  },
+  learningLanguage: {
+    type: String,
+    default: "",
+  },
+  location: {
+    type: String,
+    default: "",
+  },
+  university: {
+    type: String,
+    default: "",
+  },
+  major: {
+    type: String,
+    default: "",
+  },
+  interests: {
+    type: [String],
+    default: [],
+  },
+  studyPreferences: {
+    preferredStudyTime: {
+      type: String,
+      enum: ["Morning", "Afternoon", "Evening", "Flexible"],
+      default: "Flexible",
+    },
+    preferredStudyMode: {
+      type: String,
+      enum: ["Online", "Offline", "Hybrid"],
+      default: "Online",
+    },
+  },
+  joinedGroups: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "StudyGroup",
+      default: [],
+    },
+  ],
+  isOnboarded: {
+    type: Boolean,
+    default: false,
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
